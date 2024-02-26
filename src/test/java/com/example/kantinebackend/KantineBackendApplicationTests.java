d5e01e3e42ba34dc7e9061d192d2cb43bd9526e7
ceddyie <cedric.kessler27@gmail.com>
2024-02-26 22:43:06 +0100

    created Backend project

---
+++ src/test/java/com/example/kantinebackend/KantineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kantinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KantineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
