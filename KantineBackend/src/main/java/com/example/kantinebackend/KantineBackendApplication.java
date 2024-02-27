fb703aaf65fbf43ebc16eaf07784774d699eeb2d
Ceddyie <59868428+Ceddyie@users.noreply.github.com>
2024-02-27 23:12:32 +0100

    Add files via upload

---
+++ KantineBackend/src/main/java/com/example/kantinebackend/KantineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kantinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KantineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KantineBackendApplication.class, args);
+	}
+
+}
